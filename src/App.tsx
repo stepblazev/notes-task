b7eaa9523ba031a68a590b2e6001c6841c49df2f
SBViside <ohvatuki@gmail.com>
2023-07-21 14:46:46 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { FC } from 'react';
+import { Container, Typography } from '@mui/material';
 
 const App: FC = () => {
-	return <div></div>;
+	return (
+		<Container>
+			<Typography variant='h3' component='h1' textAlign='center'>
+				App Init
+			</Typography>
+		</Container>
+	);
 };
 
 export default App;
